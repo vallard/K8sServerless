e9fefd1dd3e4f96560774ab6aba735cd5d4baf4e
vallard <vallard@benincosa.com>
2019-01-14 15:07:30 -0800

    added some modules that weren't being tracked.

---
+++ kubeless/runtime/kubeless.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+
+import os
+import imp
+import datetime
+
+from multiprocessing import Process, Queue
+import bottle
+import prometheus_client as prom
+
+mod = imp.load_source('function',
+                      '/kubeless/%s.py' % os.getenv('MOD_NAME'))
+func = getattr(mod, os.getenv('FUNC_HANDLER'))
+func_port = os.getenv('FUNC_PORT', 8080)
+
+timeout = float(os.getenv('FUNC_TIMEOUT', 180))
+
+app = application = bottle.app()
+
+func_hist = prom.Histogram('function_duration_seconds',
+                           'Duration of user function in seconds',
+                           ['method'])
+func_calls = prom.Counter('function_calls_total',
+                           'Number of calls to user function',
+                          ['method'])
+func_errors = prom.Counter('function_failures_total',
+                           'Number of exceptions in user function',
+                           ['method'])
+
+function_context = {
+    'function-name': func,
+    'timeout': timeout,
+    'runtime': os.getenv('FUNC_RUNTIME'),
+    'memory-limit': os.getenv('FUNC_MEMORY_LIMIT'),
+}
+
+def funcWrap(q, event, c):
+    try:
+        q.put(func(event, c))
+    except Exception as inst:
+        q.put(inst)
+
+def enable_cors(fn):
+    def _enable_cors(*args, **kwargs):
+        # set CORS headers
+        bottle.response.headers['Access-Control-Allow-Origin'] = '*'
+        bottle.response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+        bottle.response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+        if bottle.request.method != 'OPTIONS':
+            # actual request; reply with the actual response
+            return fn(*args, **kwargs)
+
+    return _enable_cors
+
+
+@app.route('/', method=['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'])
+@enable_cors
+def handler():
+    req = bottle.request
+    content_type = req.get_header('content-type')
+    data = req.body.read()
+    if content_type == 'application/json':
+        data = req.json
+    event = {
+        'data': data,
+        'file': req.files.get('file'),
+        'event-id': req.get_header('event-id'),
+        'event-type': req.get_header('event-type'),
+        'event-time': req.get_header('event-time'),
+        'event-namespace': req.get_header('event-namespace'),
+        'extensions': {
+            'request': req
+        }
+    }
+    method = req.method
+    func_calls.labels(method).inc()
+    with func_errors.labels(method).count_exceptions():
+        with func_hist.labels(method).time():
+            q = Queue()
+            p = Process(target=funcWrap, args=(q, event, function_context))
+            p.start()
+            p.join(timeout)
+            # If thread is still active
+            if p.is_alive():
+                p.terminate()
+                p.join()
+                return bottle.HTTPError(408, "Timeout while processing the function")
+            else:
+                res = q.get()
+                if isinstance(res, Exception):
+                    raise res
+                return res
+
+@app.get('/healthz')
+def healthz():
+    return 'OK'
+
+@app.get('/metrics')
+def metrics():
+    bottle.response.content_type = prom.CONTENT_TYPE_LATEST
+    return prom.generate_latest(prom.REGISTRY)
+
+if __name__ == '__main__':
+    import logging
+    import sys
+    import requestlogger
+    loggedapp = requestlogger.WSGILogger(
+        app,
+        [logging.StreamHandler(stream=sys.stdout)],
+        requestlogger.ApacheFormatter())
+    bottle.run(loggedapp, server='cherrypy', host='0.0.0.0', port=func_port)
+
---
+++ serverless/photos.py
@@ -0,0 +1,150 @@
+import pymongo
+import json, os, time, base64
+
+from bson.json_util import dumps as bdumps
+from bson.objectid import ObjectId
+
+from kubernetes import client, config # to get kubernetes secrets to access mongo and minio
+
+from minio import Minio
+from minio.error import ResponseError
+
+
+MONGODB_HOST = os.environ.get("MONGODB_HOST") or "fonkdb-mongodb"
+MONGODB_PORT = os.environ.get("MONGODB_PORT") or 27017
+MONGODB_COLLECTION = os.environ.get("MONGODB_COLLECTION") or "entries"
+MONGODB_SECRETS = os.environ.get("MONGODB_SECRETS") or "fonkdb-mongodb"
+
+MINIO_HOST = os.environ.get("MINIO_HOST") or "fonkfe:9000"
+MINIO_SECRETS = os.environ.get("MINIO_SECRETS") or "fonkfe"
+MINIO_BUCKET = os.environ.get("MINIO_BUCKET") or "uploads"
+mgo_database = "photos"
+
+def setup():
+    # get kubernetes info
+    config.load_incluster_config()
+    v1=client.CoreV1Api()
+    access_key = ""
+    secret_key = ""
+    mgo_user = "root"
+    mgo_password = ""
+    # connect to KUbernetes to get secrets
+    # get the secrets to connect to the services.  Make sure your service account can do this or
+    # this will fail. 
+    for secrets in v1.list_secret_for_all_namespaces().items:
+        if secrets.metadata.name == MINIO_SECRETS:
+            access_key = base64.b64decode(secrets.data['accesskey'])
+            secret_key = base64.b64decode(secrets.data['secretkey'])
+        if secrets.metadata.name == MONGODB_SECRETS:
+            #mgo_password = base64.b64decode(secrets.data['mongodb-password'])
+            mgo_password = base64.b64decode(secrets.data['mongodb-root-password'])
+
+
+    print(access_key, secret_key)
+    print(mgo_password)
+    # try to connect to minio
+    minio = Minio(MINIO_HOST,
+                access_key=access_key,
+                secret_key=secret_key,
+                secure=False)
+
+    print(mgo_user, mgo_password, MONGODB_HOST)
+    mongo = pymongo.MongoClient("mongodb://{0}:{1}@{2}".format(mgo_user,
+                                                              mgo_password, 
+                                                               MONGODB_HOST))
+    return minio, mongo
+        
+def list(event, context):
+    """
+    List the photos with their meta data
+    """
+    try:
+        minio, mongo = setup()
+    except Exception as err:
+        return json.dumps({"setup error": str(err)})
+        
+    try:
+        collection = mongo[mgo_database][MONGODB_COLLECTION]
+        photos = [x for x in collection.find({})]
+        result = bdumps({"photos": photos})
+        return result
+    except Exception as err:
+        return json.dumps({"error": str(err)})
+
+
+def get_image(iid):
+    """
+    Get just one image entry from the database.
+    """
+    mongo, minio = setup()
+    collection = mongo[mgo_database][MONGODB_COLLECTION]
+    images = [i for i in collection.find({"_id": ObjectId(iid)})]
+    return images[0]
+
+
+
+def del(event, contextiid):
+    """
+    Remove photo from mongodb and from minio
+    """
+    iid = event["data"]["id"]
+    print("Image to delete: ", iid)
+    try: 
+        mongo, minio = setup()
+    except Exception as err:
+        return json.dumps({"setup error": str(err)})
+
+    # remove from mongo db    
+    try:
+        collection = mongo[mgo_database][MONGODB_COLLECTION]
+        image = get_image(iid)
+        result = collection.delete_one({'_id' : ObjectId(iid)})
+        print(result)
+        return result
+    except Exception as err:
+        return json.dumps({"error": str(err)})
+
+    # remove from minio
+    try:
+        minio.remove_object(MINIO_BUCKET, image["name"])
+    except ResponseError as err:
+        print(err)
+        return json.dumps({"error" : str(err)})
+
+    return list()
+
+
+def get_file_length(filepointer):
+    filepointer.seek(0, 2) # go to the end of the file pointer
+    file_length = filepointer.tell() # get the length of the file
+    filepointer.seek(0, 0) # set the file pointer back to the beginning
+    return file_length
+
+
+def upload(event, context):
+    try:
+        mongo, minio = setup()
+    except Exception as err:
+        return json.dumps({"setup error": str(err)})
+    f = event['data']['files']['file'] # get the FileStorage object from the form
+    file_length = get_file_length(f)
+    # store the image in minio
+    try:
+        minio.put_object("uploads", f.filename, f ,file_length, f.content_type)
+    except ResponseError as err:
+        print(err, file=sys.stderr)
+        return json.dumps({"error" : str(err)}), 500
+
+    # store the file metadata in mongo
+    collection = mongo["photos"]["entries"]
+
+    photo = {
+        "name" : f.filename,
+        "date" : datetime.datetime.utcnow(),
+        "url" : minio.presigned_get_object('uploads', f.filename, expires=timedelta(days=5))
+    }
+
+    # put object information into database.
+    img_id = collection.insert_one(photo).inserted_id
+    #return bdumps({"id": img_id }), 200
+    return get_images()
